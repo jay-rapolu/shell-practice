87ff1cb1d37ed4ec548b776d497f3a4629479e54
jay-rapolu <jay.rapolu@outlook.com>
2026-01-18 16:13:57 +0530

    arrays

---
+++ array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CAR_COMPANIES=("TATA" "SUZUKI" "HONDA" "HYUNDAI" "CITREON" "AUDI" "BMW" "MERCEDES" "JAGUAR" "LAND ROVER" "RANGE ROVER")
+
+echo "first name in the car companies is: ${CAR_COMPANIES[0]}"
+echo "list of all car companies are: ${CAR_COMPANIES[@]}
