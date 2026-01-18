949dafe86b041a3f6edd5d4a3909ec44543aa701
jay-rapolu <jay.rapolu@outlook.com>
2026-01-18 15:49:34 +0530

    argument-variables

---
+++ argument-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1="$1"
+PERSON2="$2"
+
+echo "${PERSON1}: Hi ${PERSON2}, how are you ?"
+echo "${PERSON2}: Hi ${PERSON1}, I'm doing good what about you?"
+echo "${PERSON1}: I'm good, any plans for this weekend."
+echo "${PERSON2}: No, did not planning anything yet."
+echo "${PERSON1}: I'm planning for a trip, will you join."
+echo "${PERSON2}: yeah, sure let us plan a trip for this weekend."
