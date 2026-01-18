e31e96463d3594a9c152aa011b0d0d4e9e24f683
jay-rapolu <jay.rapolu@outlook.com>
2026-01-18 15:54:53 +0530

    dynamic-variables

--- dynamic-variables.sh
+++ dynamic-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+TIMESTAMP=$(date) # using paranthesis to store the output of the command into a variable
 
-echo "script executed on: ${TIMESTAMP}"
+echo "script executed on: ${TIMESTAMP}" #here timestamp will be printed
 
-echo "script exected on: $date"
+echo "script exected on: $date" #here timestamp will not be printed
